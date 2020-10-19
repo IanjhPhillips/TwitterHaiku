66516da9eaa7822f19c5702ae100425111ede8d6
Ian Phillips <ianjhphillips@gmail.com>
2020-10-19 18:09:47 -0400

    Connected bot to twitter... really makes you think

--- src/bot.js
+++ src/bot.js
@@ -4,6 +4,38 @@ const Twit = require('twit');
 const config = require('./config');
 const Haiku = require ('./haiku');
 
-var T = new Twit(config);
+const T = new Twit(config);
 
-console.log(Haiku.generateHaiku());
+/*
+let userStream = T.stream('statuses/filter', { track: '@haikubot12' });
+
+userStream.on('follow', followed);
+
+function followed(eventMsg) {
+    console.log("Somebody followed me!");
+    let name = eventMsg.source.name;
+    let screenName = eventMsg.source.screen_name;
+    sendTweet('@'+screenName+
+        "\nThanks for following"+
+        "\nI hope you can stay awhile"+
+        "\nHave an awesome day!");
+}
+*/
+
+sendHaiku();
+setInterval(sendHaiku, 1000*60*60);
+
+function sendTweet (tweet) {
+
+    console.log(tweet);
+    T.post('statuses/update', {status: tweet}, function(err, data, response) {
+        if (err)
+            console.log(err);
+        else
+            console.log("Successfully posted!");
+    });
+}
+
+function sendHaiku () {
+    sendTweet(Haiku.generateHaiku().concat("Really makes you think...\n#haiku"));
+}
